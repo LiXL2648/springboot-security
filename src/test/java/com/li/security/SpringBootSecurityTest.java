b096d1fa08d15d8487ccb4d235f8421869b1ff76
LiXL <1634491328@qq.com>
2020-08-19 18:48:03 +0800

    RBAC权限管理模型

--- src/test/java/com/li/security/SpringBootSecurityTest.java
+++ src/test/java/com/li/security/SpringBootSecurityTest.java
@@ -1,6 +1,5 @@
 package com.li.security;
 
-import com.li.security.dao.UserDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +16,9 @@ public class SpringBootSecurityTest {
     @Autowired
     private DataSource dataSource;
 
-    @Autowired
-    private UserDao userDao;
-
     @Test
     public void testDataSource() throws SQLException {
         System.out.println(dataSource);
         System.out.println(dataSource.getConnection());
-    }
-
-    @Test
-    public void testGetUserById() {
-        System.out.println(userDao.getUserById(1));
     }
 }
